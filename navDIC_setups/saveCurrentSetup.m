323988f055a42a4bcd38ae754c4b34f084e0935f
MargeritPierre <margeritpierre@enpc.fr>
2018-02-07 12:05:59 +0100

    New Commit

--- navDIC_setups/saveCurrentSetup.m
+++ navDIC_setups/saveCurrentSetup.m
@@ -8,8 +8,14 @@ if ~isempty(varargin)
     frameToSave = varargin{1} ;
 end
 
-if ~isempty(hd.WorkDir) && strcmp(hd.ToolBar.MainMenu.saveImages.Checked,'on')
-    wd = hd.WorkDir ;
-    nameImg = [wd.Path,wd.CommonName,'_',num2str(frameToSave),wd.ImagesExtension] ;
-    imwrite(uint16(65535*hd.Images{frameToSave}{camID}),nameImg) ;
-end
+% Save Images
+    if ~isempty(hd.WorkDir) && strcmp(hd.ToolBar.MainMenu.saveImages.Checked,'on')
+        wd = hd.WorkDir ;
+        nameImg = [wd.Path,wd.CommonName,'_',num2str(frameToSave),wd.ImagesExtension] ;
+        imwrite(uint16(65535*hd.Images{frameToSave}{camID}),nameImg) ;
+    end
+    
+% Save Data
+    nameData = [wd.Path,wd.CommonName,'_InputData.mat'] ;
+    InputData = hd.InputData ;
+    save(nameData,'InputData') ;
