19d247872387355f4689210c4450e86affea78ea
MargeritPierre <margeritpierre@enpc.fr>
2019-01-14 16:30:45 +0100

    Curvature Computing

--- navDIC_DIC/navDIC_displacements/navDIC_fftdisp/fftDispMethod.m
+++ navDIC_DIC/navDIC_displacements/navDIC_fftdisp/fftDispMethod.m
@@ -1,10 +1,9 @@
 function MovingPoints = fftDispMethod(PtsMov,PtsRef,imgMov,imgRef,CorrSize)
 
     % PARAMETERS
-        dir = 'X' ; % displacement directions: 'both', 'X' or 'Y'
-        CorrSize = 1*[6 31] ; % Rectangular window
+        dir = 'both' ; % displacement directions: 'both', 'X' or 'Y'
+        CorrSize = 41*[1 1] ; % Rectangular window
         m = round(CorrSize/4) ; % Margin to truncate borders
-        uMax = CorrSize/4*Inf ; % Maximum allowed displacement per iteration
         FIT =   ... 'LS' ...
                  'SVD' ...
                 ; 
