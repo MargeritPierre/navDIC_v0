7cb549d72c9bce17f4ed9f7babcd8232b89603c4
nicolasDucou <34479029+nicolasDucou@users.noreply.github.com>
2021-06-30 11:36:05 +0200

    Minor changes

--- navDIC_DIC/navDIC_displacements/navDIC_fftdisp/fftDispMethod.m
+++ navDIC_DIC/navDIC_displacements/navDIC_fftdisp/fftDispMethod.m
@@ -3,7 +3,7 @@ function MovingPoints = fftDispMethod(PtsMov,PtsRef,imgMov,imgRef,CorrSize)
     % PARAMETERS
         dir = 'both' ; % displacement directions: 'both', 'X' or 'Y'
         CorrSize = 101*[1 1] ; % Rectangular window
-        m = round(CorrSize/6) ; % Margin to truncate borders
+        m = round(CorrSize/4) ; % Margin to truncate borders
         uMax = CorrSize/2 ; 30*[1 1] ; % Maximum allowed displacement per iteration
         maxImagetShift = CorrSize/2 ; % Maximum allowed imagette shift (close to image borders)
         iterateIfMaxDispHigherThan = 1 ;
