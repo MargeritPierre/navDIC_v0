15aa380c3ca15686f22f723e88fbd0662f55671b
sebastienmaitenaz <sebastien.maitenaz@gmail.com>
2021-07-28 11:04:25 +0200

    Major modifications to clean_test_measures file

--- navDIC_utils/PlotOTMarkersPosition_CommonTimeLine.m
+++ navDIC_utils/PlotOTMarkersPosition_CommonTimeLine.m
@@ -10,17 +10,17 @@ OTPos = [];
 
 %% CREATE MATRIX TO PLOT
 
-tmax = 50000;
-time = hd.AcquiredOTPositions.Time(1); %floor(hd.AcquiredOTPositions.Time(1));
+tmax = 3000;
+time = hd.CommonTimeLine(1); %floor(hd.AcquiredOTPositions.Time(1));
 nbOfPositionsSelected = 0;
-for i = 50:tmax %size(hd.AcquiredOTPositions.Time,1)
-    if not(hd.AcquiredOTPositions.Time(i) == time) %not(floor(hd.AcquiredOTPositions.Time(i)) == time)
+for i = size(hd.CommonTimeLine,2)
+    if not(hd.CommonTimeLine(i) == time) %not(floor(hd.AcquiredOTPositions.Time(i)) == time)
         nbOfPositionsSelected = 0;
-        time = hd.AcquiredOTPositions.Time(i); %floor(hd.AcquiredOTPositions.Time(i));
+        time = hd.CommonTimeLine(i); %floor(hd.AcquiredOTPositions.Time(i));
     end
     if nbOfPositionsSelected == 0
         OTPos = cat(3, OTPos,...
-            hd.AcquiredOTPositions.FilteredData(:,:,i));
+            hd.AcquiredOTPositions.InterpFilteredData(:,:,i));
         %ones(size(hd.AcquiredOTPositions.InterpFilteredData,1),1)* hd.AcquiredOTPositions.Time(i)
         nbOfPositionsSelected = nbOfPositionsSelected + 1;
     end
@@ -44,7 +44,7 @@ dNorm = dNorm./max(dNorm);
 
 %% GENERATE PLOT
 
-f1 = figure;
+f2 = figure;
 set(gca, 'DataAspectRatio', [1,1,1]);
 xlabel('x(mm)')
 ylabel('z(mm)')
@@ -52,13 +52,13 @@ title('Optitrack Marker Positions \& 2D Displacement');
 subtitle('! Only 2D displacement are computed and plotted on this graph','FontSize',14,'Color',[0.87 0.34 0.28]);
 lightBlue = [104 188 227]/255;
 darkBlue = [11 82 115]/255;
-for i = 1:size(OTPos,1)
+for i = 1:(size(OTPos,1)-1)
     scatter(OTPos(i,3,1),OTPos(i,2,1),50,'red','*')
     c = [lightBlue(1) + (darkBlue(1) - lightBlue(1))* dNorm(:,i)...
         lightBlue(2) + (darkBlue(2) - lightBlue(2))* dNorm(:,i)...
         lightBlue(3) + (darkBlue(3) - lightBlue(3))* dNorm(:,i)];
     scatter(OTPos(i,3,2:size(OTPos,3)),OTPos(i,2,2:size(OTPos,3)),50,c,'.')
     [M, I] = max(d(:,i));
-    text(double(OTPos(i,3,I+1)),double(OTPos(i,2,I+1)),strcat(num2str(d(I,i)*1000,'%.1f'),' mm $\rightarrow$'), 'HorizontalAlignment', 'right','FontSize',8);
-    text(double(OTPos(i,3,1)), double(OTPos(i,2,1)) + 0.05 ,strcat('M:',' ', num2str(hd.AcquiredOTPositions.ID(i,1))), 'HorizontalAlignment', 'right','fontweight','bold','FontSize',8);
+    text(double(OTPos(i,3,I+1)),double(OTPos(i,2,I+1)),strcat(num2str(d(I,i),'%.1f'),' mm $\rightarrow$'), 'HorizontalAlignment', 'right','FontSize',8);
+    text(double(OTPos(i,3,1)), double(OTPos(i,2,1)) + 0.05 ,strcat('M:',' ', num2str(hd.AcquiredOTPositions.InterpID(i,1))), 'HorizontalAlignment', 'right','fontweight','bold','FontSize',8);
 end
